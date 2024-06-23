51be076921551f32b87b39493b322a95f4583b4b
mohammed <zeinaboy@gmail.com>
2024-06-23 18:57:24 +0100

    add project files

---
+++ src/store/routers/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { register, login } = require('../controllers/userController');
+const router = express.Router();
+const { check } = require('express-validator');
+
+router.post('/register', [
+    check("username").isLength({ min: 3 }).withMessage('Username must be atleast 3 characters long'),
+    check("email").isEmail().withMessage('Please enter a valid email'),
+    check("password").isLength({ min: 6 }).withMessage('Password must be atleast 6 characters long')
+], register);
+
+router.post('/login', [
+    check("email").isEmail().withMessage('Please enter a valid email'),
+    check('password').not().isEmpty().withMessage('Password is required')
+], login);
+
+
+module.exports = router;
